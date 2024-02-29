00daa1a86131d46b8e3e2706e9c3790b76ff6822
callmezydd <zudapradanaputra.17@fasilkom.narotama.ac.id>
2024-02-29 18:31:39 +0700

    Delete unnecessary file

--- src/main/java/org/zydd/controllers/ICustomer.java
+++
@@ -1,8 +0,0 @@
-package org.zydd.controllers;
-
-import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
-import org.zydd.models.Customer;
-
-public interface ICustomer extends PanacheEntityResource<Customer, Long> {
-
-}
